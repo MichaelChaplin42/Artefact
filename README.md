b116229dab67cad44d094cbb192e17f98e6dfd29
MichaelChaplin42 <c017813j@student.staffs.ac.uk>
2023-02-19 18:20:58 +0000

    Adds the check System

--- README.md
+++ README.md
@@ -5,10 +5,16 @@ Can create dictionary with count
     count = 2
     starttime = 17:20:00
 upon packet recieve
-check if ip is logged
-if no log ip, time,count =1
-if yes check start time is > 1 minute from current time
-    if yes reset count and start time
-    if no add 1 to count and check if count is greater than threshold
-        if yes add to black list
-        if no continue
+check if ip is logged - done
+if no log ip, time,count =1 - done
+if yes check start time is > 1 minute from current time - done
+    if yes reset count and start time - done
+    if no add 1 to count and check if count is greater than threshold - done
+        if yes add to black list - done
+        if no continue - done
+
+need to iterate through entire dictionary before adding to dictionary
+
+Bodge fixes to find better 
+- iterate through dict - created blank entry to make it start
+- add new entry to dict - made it check entire dict before entering through true/false condition
--- artefact.py
+++ artefact.py
@@ -3,6 +3,13 @@ from scapy.all import *
 from netaddr import *
 import os
 import time
+Threshold = 20
+
+info = {"Vectors": [{
+    'IP': 0,
+    'Count': 0,
+    'startTime': 0
+}]}
 def firstStart():
     try:
         os.system("ipset creat black_list hash:ip")
@@ -12,22 +19,49 @@ def firstStart():
         os.system("iptables -A INPUT -m set --match-set black_list src -j DROP")
     except:
         print("IPTABLES RULE EXISTS")
+#Creates iptables rule and ipset on first start
+def ipFound(dict):
+    dict.update({'Count':dict['Count']+1})
+    print("COUNT UPDATED")
+    diff = time.time() - dict['startTime']
+    if diff > 60:
+        dict.update({'Count':1})
+        dict.update({'startTime':time.time()})
+    else:
+        if dict['Count'] >= Threshold:
+            print("Greater than threshold Blocking")
+            #dict.update({'Blocked':True})
+            #os.system("ipset add black_list "+dict['IP'])
+
+
 def pktHandle(i):
         if i.haslayer("IP"):
             sourceIP = i["IP"].src
         else:
             sourceIP = i.src
-        #if sourceIP == "172.217.169.46":
-            #print("SENT PACKET")
-        #else:
-            #print("Recieved Packet")
         if sourceIP == "192.168.129.191":
             print("PING DETECTED")
-            os.system("ipset add black_list "+sourceIP)
-            time.sleep(10)
-            os.system("ipset del black_list "+sourceIP)
-            exit()
-        print(sourceIP)
+            ipPresent = False
+            for i in info["Vectors"]: # iterates through dictionary
+                if i['IP']== sourceIP: # if ip is found
+                    ipPresent = True
+                    if i['Blocked'] == False:
+                        print("True")
+                        ipFound(i)
+                        
+                        break
+            
+            if ipPresent == False:   #Below creates an entry into the dictionaryj with the ip time and count if the ip is not found within the dictionary    
+                data = {
+                'IP': sourceIP,
+                'Count': 1,
+                'startTime': time.time(),
+                'Blocked': False
+                }
+                info["Vectors"].append(data)
+            
+            print(info)
+
 
 
 
@@ -51,4 +85,5 @@ while run == True:
     elif ans == 2:
         scTest()
     else:
+        os.system("ipset del black_list 192.168.129.191")
         exit()
