31f8aac1ab79d0de46af90eb9acdb088c475e3b9
MCHAPLIN <c017813j@student.staffs.ac.uk>
2023-03-12 18:16:49 +0000

    afdsf

--- checks.py
+++ checks.py
@@ -1,34 +1,55 @@
 #!/usr/bin/env python3
+
+# Import necessary modules
 import json
 import time
 import subprocess
 from datetime import date
+from scapy.all import *
+
+# Load the previous records from a JSON file
 f = open('Record.json')
 record = json.load(f)
 f.close()
 
-#for every packet checks 
+# Define a function to track incoming packets
 def track(pkt):
+
+    # Set the threshold for attack detection
     threshold = 20000
+
+    # Check if the packet has an IP layer
     if pkt.haslayer("IP"):
+
+        # Get the source IP address of the packet
         sourceIP = pkt["IP"].src
-        if sourceIP == "192.168.139.110":
+
+        # If the packet was sent from this computer, ignore it
+        if sourceIP == get_if_addr(conf.iface):
             pass
         else:
+            # Check if the system has any records
             if record['Records']:
                 ipfound = False
+                # Loop through the previous records to find the source IP address
                 for i in record['Records']:
                     if i['IP'] == sourceIP:
                         ipfound = True
+                        # If the IP address has not been blocked
                         if i['Blocked'] == False:
+                            # Increase the packet count for the IP address
                             i.update({'Count':i['Count']+1})
+                            # If more than 1 minute has passed since the last count reset
                             if time.time()-i['startTime'] > 60:
+                                # If the packet count is above the threshold, return the IP address for further processing
                                 if i['Count'] > threshold :
                                     return(i)
+                                # If the packet count is below the threshold, reset the count and start a new timer
                                 elif i['Count'] < threshold:
                                     i.update({'Count':1})
                                     i.update({'startTime':time.time()})
                         break
+                # If the source IP address is not found in previous records, add a new record for it
                 if ipfound == False:
                     data = {
                             'IP': sourceIP,
@@ -37,6 +58,7 @@ def track(pkt):
                             'Blocked': False
                         }
                     record['Records'].append(data)
+            # If there are no previous records, add a new record for the source IP address
             else:
                 data = {
                     'IP': sourceIP,
@@ -47,47 +69,60 @@ def track(pkt):
                 record['Records'].append(data)
     else:
         pass
+    # Return "Clear" if no IP address is returned for further processing
     return("Clear")
 
+# Define a function to write the updated records to a JSON file
 def log(record):
     json_object = json.dumps(record, indent=4)
     with open("Record.json","w") as file:
         file.write(json_object)
+    # Reload the updated records from the JSON file
     f = open('Record.json')
     record = json.load(f)
     f.close()
 
+# Define a function to log an incident in a text file
 def incidentLog(ip,blocked):
     with open("LOG.txt",'a+') as f:
+        # Get the current time and date
         t = time.time()
         currentTime = time.strftime("%H:%M:%S",time.gmtime(t))
+        # Format and write the incident log message to the text file
         info = str(date.today())+": "+ip['IP']+" Flagged at "+str(currentTime)+" With "+str(ip['Count'])+' Packets per minute. INVESTIGATING'
         f.write(info+" "+str(blocked)+'\n')
 
+# Define a function to check for volumetric attacks
 def volAttackCheck(ip,record):
     isAttack = False
-    
+    # Check if the IP has sent more than a set threshold packets in the last minute
     if ip['Count'] > 100:
         isAttack == True
+        # Add the IP to the blacklist using the 'ipset' command
         subprocess.check_call(['ipset','add','black_list',ip['IP']])
+        # Log the incident in the 'LOG.txt' file
         incidentLog(ip,blocked=True)
+    # Check if the IP is already in the record dictionary
     for i in record['Records']:        
         if ip['IP'] == i['IP']:
+            # If the IP is not an attacker, update the count and start time
             if isAttack == False:
                 i.update({'Count':1})
                 i.update({'startTime':time.time()})
+            # If the IP is an attacker, mark it as blocked
             elif isAttack == True:
                 i.update({'Blocked':True})
 
 def thresholdLog():
+    # Load the contents of the JSON file into a dictionary
     with open('Record.json', 'r') as file:
         data = json.load(file)
 
-# Open a new text file for writing
+    # Load the contents of a different JSON file into a dictionary
     with open('data.json', 'r') as file:
         data2 = json.load(file)
+    # For each record in the first JSON file, add a new record to the second JSON file
     for record in data['Records']:
-        
         count = record['Count']
         ip = record['IP']
         new = {
@@ -95,5 +130,6 @@ def thresholdLog():
                 'Count':count
             }
         data2['Records'].append(new)
+    # Overwrite the contents of the second JSON file with the updated dictionary
     with open('data.json', 'w') as file:
         json.dump(data2, file,indent=4)
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
+
+# Import necessary modules
 from scapy.all import sniff
 import subprocess
 import checks
 import time
 
+# Define function to exit program
 def exit():
     print("EXITING")
     quit()
 
+# Function to run first setup 
 def firstSetup():
     print("Running First Setup")
+    # Update packages and install necessary packages
     subprocess.check_call(['apt-get','update'])
     subprocess.check_call(['apt-get','upgrade','-y'])
     subprocess.check_call(['pip','install','--pre','scapy[basic]'])
     subprocess.check_call(['apt','install','ipset'])
+    # Create IP set if it does not exist and add it to iptables
     try:
         subprocess.check_call(['ipset','create','black_list','hash:ip'])
     except:
@@ -23,35 +29,45 @@ def firstSetup():
     except:
         print("IPTABLES RULE EXISTS")
 
+
+# Define function to start sniffing traffic
 def sniffer():
     print("SNIFFING")
     global run 
     run = time.time() + 36000
     sniff(prn=pkthandle)
 
+# Function to handle each packet
 def pkthandle(pkt):
     global logtime
     global logtime2
     sec = int(run-time.time())
     sec1 = sec % 60
     min = sec / 60 
+    # Print time remaining before program exits due to time limit Just for testing
     print(int(min), "Minutes and",sec1,"Seconds left")
+    # Exit program if time limit is reached
     if sec < 0:
         exit()
+    # Logs the information that the system is actively tracking every 60 seconds
     if (time.time()-logtime) >60:
         logtime = time.time()
         checks.log(checks.record)
+    # Create a backup log of the current log every 600 seconds that does not get replaced 
     if (time.time()-logtime2) > 600:
         logtime2 = time.time()
         checks.thresholdLog()
     ipcheck = checks.track(pkt)
+    # If packet is flagged as a potential attack, log it and check for a volumetric attack
     if ipcheck != "Clear":
         checks.incidentLog(ipcheck,blocked=False)
         checks.volAttackCheck(ipcheck,checks.record)
-
+# Function for testing purposes only
 def testing():
     print("TESTING")
 
+
+# Start loop to prompt user for input and run appropriate functions
 run = True
 print("AUTOMATED DDOS PROTECTION TOOL")
 while run == True:
